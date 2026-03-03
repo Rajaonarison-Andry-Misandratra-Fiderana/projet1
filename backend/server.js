e2851ca9245f2dfc72ae3f49e7d0b5eb4afde8ac
fiderana <fideranamgaa@gmail.com>
2026-03-03 11:39:25 +0300

    fix du serveur

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const connectDB = require("./config/db");
 connectDB();
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//Route
-app.get("/", (req, res) => {
-  res.send("serveur fonctionne");
-});
-
+// Routes API
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
@@ -23,8 +20,21 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-//Demarage du serveur
+// 🔥 Servir Angular
+const angularPath = path.join(
+  __dirname,
+  "../frontend/shopping-mall/dist/shopping-mall/browser",
+);
+
+app.use(express.static(angularPath));
+
+// 🔥 Fallback SPA (IMPORTANT : toujours en dernier)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(angularPath, "index.html"));
+});
+
+// Démarrage serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Serveur fonctionne sur le port ${process.env.PORT || 5000}`);
+  console.log(`Serveur fonctionne sur le port ${PORT}`);
 });
